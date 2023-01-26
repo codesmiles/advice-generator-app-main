1a8c4f74e99891c2935973c5db1a7ceac291bb93
Nwogu Michael <mikedbchi@gmail.com>
2023-01-26 21:26:34 +0100

    Update

--- advice-generator-app/src/views/HomeView.vue
+++ advice-generator-app/src/views/HomeView.vue
@@ -1,43 +1,59 @@
 <template>
-  <main class="bg-[#1f2632] h-screen flex justify-center items-center text-lg  font-manrope">
-    <div class=" bg-[#323A49] rounded-lg w-11/12 relative max-w-lg">
-      <h1 className="text-[#52ffa8] text-center py-5 uppercase text-sm tracking-widest">
-        advice #{{ adviceTitle }}
-      </h1>
-      <p class="text-[#cee3e9] text-center tracking-wide font-black sm:px-10"> {{ adviceDescription }}</p>
+  <main
+    class="bg-[#1f2632] h-screen flex justify-center items-center text-lg font-manrope"
+  >
+    <div class="bg-[#323A49] rounded-lg w-11/12 relative max-w-lg">
+      <!-- loader -->
+      <div v-show="isLoading" class="my-10 flex justify-center">
+        <Loader/>
+      </div>
+      <!--  -->
+      <div v-show="!isLoading">
+        <h1
+          className="text-[#52ffa8] text-center py-5 uppercase text-sm tracking-widest"
+        >
+          advice #{{ adviceDetails.titleNumber }}
+        </h1>
+        <p class="text-[#cee3e9] text-center tracking-wide font-black sm:px-10">
+          {{ adviceDetails.description }}
+        </p>
+      </div>
       <div class="mt-5 mb-16">
-        <img :src="dividerImg" alt="divider" class="mx-auto"/>
+        <img :src="dividerImg" alt="divider" class="mx-auto" />
       </div>
-      <div @click="handleClick" class="bg-[#52ffa8] absolute p-4 rounded-full left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:bg-[#cee3e9] hover:rotate-180 transition duration-500 ease-in-out hover:scale-110 ">
-        <img src="../assets/images/icon-dice.svg"  alt="dice">
+      <div
+        @click="handleClick"
+        class="bg-[#52ffa8] absolute p-4 rounded-full left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer hover:bg-[#cee3e9] hover:rotate-180 transition duration-500 ease-in-out hover:scale-110"
+      >
+        <img src="../assets/images/icon-dice.svg" alt="dice" />
       </div>
     </div>
-    <!-- loader -->
-    <div>
-      
-      <!-- <Loader/> -->
-    </div>
-<!--  -->
   </main>
 </template>
 <script>
-import { onMounted, ref } from 'vue'
+import { ref, onMounted, reactive } from "vue";
 import dividerMobile from "../assets/images/pattern-divider-mobile.svg";
 import dividerDesktop from "../assets/images/pattern-divider-desktop.svg";
 import Loader from "../components/Loader.vue";
 export default {
   components: {
-    Loader
+    Loader,
   },
   setup() {
-    const adviceTitle =ref(117);
-    const adviceDescription = ref('It is easy to sit up and take notice, What is difficult is getting up and taking action.')
+    const adviceDetails = reactive({
+      titleNumber: ref(117),
+      description: ref(
+        "It is easy to sit up and take notice, What is difficult is getting up and taking action."
+      ),
+    });
+
     // capture window screen size
     const windowWidth = ref(window.innerWidth);
 
-    const dividerImg = ref(dividerMobile);
+    const isLoading = false;
+    const dividerImg = ref("");
 
-    onMounted(()=> {
+    onMounted(() => {
       if (windowWidth.value > 768) {
         dividerImg.value = dividerDesktop;
       } else {
@@ -47,33 +63,32 @@ export default {
       // fetch("https://api.adviceslip.com/advice")
       //   .then(res => res.json())
       //   .then(val => {
-      
+      //   return (
+      //       (adviceTitle.value = val.id), (adviceDescription.value = val.advice)
+      //     );
       //   })
-      
-})
+    });
 
-
-    const handleClick = () => {
-      // fetch("https://api.adviceslip.com/advice")
-      //   .then(res => res.json())
-      //   .then(val => {
-      
-      //   })
+    const handleClick = async () => {
+      // await fetch("https://api.adviceslip.com/advice")
+      //   .then((res) => res.json())
+      //   .then((val) => {
+      //     return (
+      //       (adviceTitle.value = val.id), (adviceDescription.value = val.advice)
+      //     );
+      //   });
       adviceTitle.value = 201;
-      adviceDescription.value = 'clicked'
-      console.log('clicked')
-      
-
-    }
+      adviceDescription.value = "clicked";
+      console.log("clicked");
+    };
 
     return {
-      adviceTitle,
-      adviceDescription,
+      adviceDetails,
+      isLoading,
       dividerImg,
       windowWidth,
       handleClick,
-
-    }
+    };
   },
-}
+};
 </script>
